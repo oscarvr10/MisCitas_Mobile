be1278ef0bdd8ef9c751db54b79748358e758f65
Oscar Vera <vera_reyes-10@hotmail.com>
2021-03-15 20:04:13 -0600

    Ajustes en navegacion entre pantallas login y registro

--- app/src/main/java/com/ronalverey/myappointments/RegisterActivity.kt
+++ app/src/main/java/com/ronalverey/myappointments/RegisterActivity.kt
@@ -1,12 +1,22 @@
 package com.ronalverey.myappointments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ronalverey.myappointments.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvGoToLogin.setOnClickListener { view ->
+            val intent = Intent(view.context, MainActivity::class.java)
+            startActivity(intent)
+        }
 
         if (supportActionBar != null) {
             supportActionBar!!.hide()
