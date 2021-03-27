b414933733b80697c34eff9e88742f8a5defdab9
Oscar Vera <vera_reyes-10@hotmail.com>
2021-03-26 22:01:09 -0600

    Added interceptor for debugging web service calls

--- app/src/main/java/com/ronalverey/myappointments/io/ApiService.kt
+++ app/src/main/java/com/ronalverey/myappointments/io/ApiService.kt
@@ -1,29 +1,42 @@
 package com.ronalverey.myappointments.io
 
 import com.ronalverey.myappointments.model.Doctor
+import com.ronalverey.myappointments.model.Schedule
 import com.ronalverey.myappointments.model.Specialty
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService {
     @GET("specialties")
-    abstract fun getSpecialties(): Call<ArrayList<Specialty>>
+    fun getSpecialties(): Call<ArrayList<Specialty>>
 
     @GET("specialties/{specialty}/doctors")
-    abstract fun getDoctorsBySpecialty(@Path("specialty") specialtyId: Int): Call<ArrayList<Doctor>>
+    fun getDoctorsBySpecialty(@Path("specialty") specialtyId: Int): Call<ArrayList<Doctor>>
+
+    @GET("schedule/hours")
+    fun getAvailableHours(@Query("doctor_id") doctorId: Int, @Query("date") date: String): Call<Schedule>
 
     companion object Factory {
         private const val BASE_URL = "https://test-appointments.azurewebsites.net/api/"
         //private const val BASE_URL = "http://my-appointments.test/api/"
 
         fun create() : ApiService {
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+            val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
             val retrofit = Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
                     .build()
+
             return retrofit.create(ApiService::class.java)
         }
     }
