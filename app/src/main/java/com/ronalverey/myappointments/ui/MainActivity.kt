11341ece7d19bdb33cfb38fb17eee5be857c66a4
Oscar Vera <vera_reyes-10@hotmail.com>
2021-05-08 17:08:54 -0500

    Implementacion inicial de push notifications con FCM

--- app/src/main/java/com/ronalverey/myappointments/ui/MainActivity.kt
+++ app/src/main/java/com/ronalverey/myappointments/ui/MainActivity.kt
@@ -2,9 +2,12 @@ package com.ronalverey.myappointments.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.messaging.FirebaseMessaging
 import com.ronalverey.myappointments.PreferenceHelper.defaultPrefs
 import com.ronalverey.myappointments.PreferenceHelper.get
 import com.ronalverey.myappointments.PreferenceHelper.set
@@ -33,6 +36,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            val msg = getString(R.string.msg_token_fcm, token)
+            Log.d(TAG, msg)
+        })
 
         val prefs = defaultPrefs(this)
         if (prefs["jwt", ""].contains("."))
@@ -109,5 +126,9 @@ class MainActivity : AppCompatActivity() {
             super.onBackPressed()
         else
             snackbar.show()
+    }
+
+    companion object {
+        private const val TAG = "FCMService"
     }
 }
